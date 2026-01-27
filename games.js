9f6c670c452e652ef8996b921f3f7bcd5e8b0605
samkarikalan <karikalan.indo@gmail.com>
2026-01-27 09:36:29 +0900

    Add files via upload

--- games.js
+++ games.js
@@ -150,7 +150,7 @@ function overlapsTooMuch(pair1, pair2, lastGame) {
 }
 
 
-function 123AischedulerNextRound(schedulerState) {
+function AischedulerNextRound(schedulerState) {
   const {
     activeplayers,
     numCourts,
@@ -348,7 +348,7 @@ function 123AischedulerNextRound(schedulerState) {
   };
 }
 
-function AischedulerNextRound(schedulerState) {
+function bkAischedulerNextRound(schedulerState) {
   const {
     activeplayers,
     numCourts,
